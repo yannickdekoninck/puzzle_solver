20e3855a4dcedffedc7e6e12a1243e849ac12238
Yannick De Koninck <yannick.dekoninck@gmail.com>
2022-08-22 13:01:29 +0200

    Solver definition

--- PuzzleSolver/Board.cpp
+++ PuzzleSolver/Board.cpp
@@ -23,3 +23,20 @@ bool Board::put_piece(const PieceView &piece_view, int x, int y)
     }
     return true;
 }
+
+bool Board::solve()
+{
+    // Step 1 find the most difficult position to place
+
+    // Step 2 loop through the pieces that can sattisfy this placement
+
+    // Step 2.1 loop through the piece views of the selected piece
+
+    // Step 2.2 try to place every piece view
+
+    // Step 2.3 if piece view fits -> recursively solve again but this piece removed from the list
+
+    // How to keep track of which pieces can still be used and which ones can't?
+
+    return true;
+}
--- PuzzleSolver/Board.h
+++ PuzzleSolver/Board.h
@@ -1,12 +1,17 @@
 #pragma once
-
+#include <vector>
 #include "GridView.h"
 #include "Piece.h"
 
 class Board : public GridView
 {
 public:
-    Board(int width, int height) : GridView(width, height) {}
+    Board(int width, int height, std::vector<Piece *> &pieces) : GridView(width, height), pieces(pieces) {}
 
     bool put_piece(const PieceView &piece_view, int x, int y);
+
+    bool solve();
+
+private:
+    std::vector<Piece *> pieces;
 };
