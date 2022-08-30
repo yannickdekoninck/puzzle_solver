db095b95fd3ce266d86379183e1e4f9d9b9ea48c
Yannick De Koninck <yannick.dekoninck@gmail.com>
2022-08-30 14:49:54 +0200

    Parsing inputs

--- PuzzleSolver/PuzzleSolver.cpp
+++ PuzzleSolver/PuzzleSolver.cpp
@@ -17,113 +17,115 @@ void print_usage()
 	std::cout << usage_string << std::endl;
 }
 
-int main(int argc, char **argv)
+PuzzleSolverApp::PuzzleSolverApp(int argc, char **argv) : board_width(11), board_height(5)
 {
+	// Initialize basics
+	board = nullptr;
+	max_solutions = 10;
+	// parse inputs
 
-	// Parse CLI arguments
-	// Let's assume 2 cases for now:
-	// - case 1: there's no CLI arguments -> empty board
-	// - case 2: the CLI arguments are of the form:
-	//   -i filename.txt
-	// And this filename contains an ascii representation of the board.
-	// Extract filename of initial board position
-	// Load file into board
-	// Check if board is valid
-	// Solve puzzle
-	// Print out result
-	// Loading the board
-
-	Board *bp;
-	Pieces p;
-	int width = 11;
-	int height = 5;
-
-	std::cout << "Welcome to PuzzleSolver!" << std::endl
-			  << std::endl;
-	if (argc < 2)
+	for (int i = 1; i < argc; i++)
 	{
-		bp = new Board(width, height, p.all_pieces);
+		// Load from file
+		if (strcmp(argv[i], "-i") == 0)
+		{
+			if (i + 1 >= argc)
+			{
+				// Not the right arguments
+				print_usage();
+				return;
+			}
+			const char *filename = argv[i + 1];
+
+			std::string board_string = load_board_from_file(filename);
+			if (board_string.size() != (size_t)(board_width * board_height))
+			{
+				std::cout << "ERROR: Board size is not the expected " << board_width << "x" << board_height << "items.";
+				return;
+			}
+			board = new Board(board_width, board_height, p.all_pieces, board_string.c_str());
+			if (board->validate_board() == false)
+			{
+				// Free board memory because it is invalid
+				delete board;
+				print_usage();
+				return;
+			}
+			i++;
+			continue;
+		}
+		// Set number of solutions
+		if (strcmp(argv[i], "-c") == 0)
+		{
+			if (i + 1 >= argc)
+			{
+				// Not the right arguments
+				std::cout << "Can't set solution count - defaulting to " << max_solutions << std::endl;
+				continue;
+			}
+			int proposed_max_solutions;
+			try
+			{
+				proposed_max_solutions = atoi(argv[i + 1]);
+			}
+			catch (...)
+			{
+				std::cout << "Can't set solution count - defaulting to " << max_solutions << std::endl;
+				continue;
+			}
+			max_solutions = proposed_max_solutions;
+			i++;
+		}
 	}
-	else if (argc == 3)
+
+	// Set board if it was not loaded through a file
+	if (board == nullptr)
 	{
-		if (!(strcmp(argv[1], "-i") == 0))
-		{
-			print_usage();
-			return 0;
-		}
-		// Load file
-		const char *filename = argv[2];
-
-		std::string board = load_board_from_file(filename);
-		if (board.size() != (size_t)(width * height))
-		{
-			std::cout << "ERROR: Board size is not the expected " << width << "x" << height << "items.";
-		}
-		bp = new Board(width, height, p.all_pieces, board.c_str());
-		if (bp->validate_board() == false)
-		{
-			print_usage();
-			return 0;
-		}
+		board = new Board(board_width, board_height, p.all_pieces);
 	}
-	else
-	{
-		print_usage();
-		return 0;
-	}
+}
 
-	// Let's create a reference to bp because that's a bit easier to work with
-	Board &b = *bp;
+PuzzleSolverApp::~PuzzleSolverApp()
+{
+	delete board;
+}
 
+bool PuzzleSolverApp::initialized()
+{
+	// App is properly initialized if the board is not a nullpointer
+	return (board != nullptr);
+}
+
+void PuzzleSolverApp::run()
+{
 	std::cout << "Here's the starting board" << std::endl
 			  << std::endl;
-
-	/*
-	for (int i = 0; i < p.all_pieces.size(); i++)
-	{
-		std::cout << "Piece number: " << i;
-		std::cout << "Number of views: " << p.all_pieces[i]->number_of_views() << std::endl;
-		for (int j = 0; j < p.all_pieces[i]->number_of_views(); j++)
-		{
-			std::cout << "View number " << j;
-			print_board(p.all_pieces[i]->get_view(j)->grid_view);
-			std::cout << std::endl;
-		}
-	}*/
-
-	// b.put_piece(*p.long_l.get_view(6), 0, 0);
-	// b.put_piece(*p.small_triangle.get_view(2), 0, 3);
-	// b.put_piece(*p.short_l.get_view(3), 1, 1);
-
-	// p.long_l.placed = true;
-	// p.small_triangle.placed = true;
-	// p.short_l.placed = true;
-
 	std::cout << "Input sequence: " << std::endl;
-	print_board(b);
-	b.update_open_neighbours();
+	print_board(*board);
+	board->update_open_neighbours();
 	auto start = std::chrono::high_resolution_clock::now();
-	b.clear_solutions();
-	b.solve();
+	board->clear_solutions();
+	board->solve();
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "Solved puzzle in " << duration.count() << "ms using " << b.tries << " iterations" << std::endl;
+	std::cout << "Solved puzzle in " << duration.count() << "ms using " << board->tries << " iterations" << std::endl;
 	std::cout << std::endl;
-	std::cout << "Found " << b.solution_count() << " solutions." << std::endl;
-	for (unsigned int i = 0; i < b.solution_count(); i++)
+	std::cout << "Found " << board->solution_count() << " solutions." << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+
+	PuzzleSolverApp p(argc, argv);
+
+	if (p.initialized() == false)
 	{
-		std::cout << "Solution: " << i << ":" << std::endl;
-		GridView *solution = b.get_solution(i);
-		print_board(*solution);
-		std::cout << std::endl;
+		return 0;
 	}
 
-	std::cout << "Done!" << std::endl;
-
-	// Clean up
-	delete bp;
+	p.run();
 
 	return 0;
 }
--- PuzzleSolver/PuzzleSolver.h
+++ PuzzleSolver/PuzzleSolver.h
@@ -4,5 +4,30 @@
 #pragma once
 
 #include <iostream>
+#include "Board.h"
+#include "Pieces.h"
 
-// TODO: Reference additional headers your program requires here.
+class PuzzleSolverApp
+{
+
+public:
+    // Constructor taking app arguments
+    PuzzleSolverApp(int argc, char **argv);
+    ~PuzzleSolverApp();
+
+    void run();
+    bool initialized();
+
+private:
+    // UI settings
+
+    int max_solutions;
+
+    int board_width;
+    int board_height;
+    // The board to play
+    Board *board;
+
+    // The pieces we use
+    Pieces p;
+};
